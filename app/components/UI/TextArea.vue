9b1860c8d8956e7efe2972597ae1c870db0de12a
xcKartoplya <bel-kir05@mail.ru>
2025-12-08 22:35:00 +0800

    added UITextArea

---
+++ app/components/UI/TextArea.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+defineProps({
+	placeholder: String,
+})
+
+const model = defineModel<string>()
+</script>
+
+<template>
+	<textarea
+		v-model="model"
+		:placeholder="placeholder"
+		class="custom-textarea"
+	></textarea>
+</template>
+
+<style lang="scss" scoped>
+.custom-textarea {
+	width: 100%;
+	height: 8rem;
+	padding: 1rem;
+	border-radius: 10px;
+	background-color: $bg-grey;
+	&:focus {
+		outline: none;
+	}
+}
+</style>
